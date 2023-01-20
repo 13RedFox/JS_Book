172a31a9581ad530e712bd56779b0f19116933c4
Serhii Shabala <sergei181989@gmail.com>
2023-01-20 15:09:10 +0200

    :sparkles: Added file 4.js (Variables)

--- 1/README.md
+++ 1/README.md
@@ -1,3 +1,4 @@
 # Основы JavaScript.
 
 - [1.1 - Привет, мир!](https://github.com/13RedFox/JS_Book/blob/main/1/js/1.js 'Привет, мир!')
+- [1.4 - Переменные](https://github.com/13RedFox/JS_Book/blob/main/1/js/4.js 'Переменные')
---
+++ 1/js/4.js
@@ -0,0 +1,23 @@
+'use strict';
+
+// Переменные:
+
+/*  
+В JavaScript есть два ограничения, касающиеся имён переменных:
+- Имя переменной должно содержать только буквы, цифры или символы $ и _.
+- Первый символ не должен быть цифрой.
+*/
+
+// Task 1
+
+let admin;
+let name;
+
+name = 'John';
+admin = name;
+alert(admin);
+
+// Task 2
+
+const ourPlanetName = 'Earth';
+const currentUserName = 'Alex';
